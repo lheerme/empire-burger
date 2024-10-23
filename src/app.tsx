8149a7161c84ad94eb2da8552741e5474d61d407
Guilherme Souza <gsouza2651@gmail.com>
2024-10-23 15:21:37 -0300

    add tanstack query

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/header";
 import { HeroSection } from "./components/hero-section";
 import { OpeningHours } from "./components/opening-hours";
 import { SpecialOffers } from "./components/special-offers";
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <main>
         <HeroSection />
         <SpecialOffers />
         <OpeningHours />
       </main>
-    </>
+    </QueryClientProvider>
   )
 }
